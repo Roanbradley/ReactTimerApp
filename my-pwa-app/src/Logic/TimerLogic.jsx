07e0c6c7a577279f935234176097ae6217957bf6
Roanbradley <roanbradley5@gmail.com>
2026-03-16 17:51:59 +0000

    added start and puase function

--- my-pwa-app/src/Logic/TimerLogic.jsx
+++ my-pwa-app/src/Logic/TimerLogic.jsx
@@ -1,31 +1,38 @@
 import { useState, useRef, useEffect } from "react";
 
-
-
-export default function useCountDown(){
+export function useCountDown(){
     
-const [SecondsLeft, setSecondsLeft] = useState(0);
-
+const [secondsLeft, setSecondsLeft] = useState(60);
+const [running, setIsRunning] = useState(false);
 
 useEffect(() => {
 
-    if(SecondsLeft <= 0) return;
+    if(!running == true || secondsLeft <= 0) return;
 
     const timeout = setTimeout(() =>{
-    setSecondsLeft(SecondsLeft - 1)
+    setSecondsLeft(secondsLeft - 1)
     },1000)
     
     return ()=> clearTimeout(timeout);
-    },[SecondsLeft]);
+    },[secondsLeft, running]);
 
+    return {secondsLeft, setIsRunning, running, setSecondsLeft};
 
-    function start(seconds)
-    {
-        setSecondsLeft(seconds);
-    }
-
-    return {SecondsLeft, start};
 }
+
+
+export function startTimer(setSecondsLeft, setIsRunning, seconds ){
+setSecondsLeft(seconds);
+setIsRunning(true);
+}
+
+export function pauseTimer(setIsRunning){
+setIsRunning(false);
+}
+
+
+
+
 
 
 
--- my-pwa-app/src/pages/Timer.jsx
+++ my-pwa-app/src/pages/Timer.jsx
@@ -1,9 +1,17 @@
+import {pauseTimer, startTimer, useCountDown} from "../Logic/TimerLogic";
+import { useState, useEffect } from "react";
+
 
 export default function Timer(){
+
+   const {secondsLeft, setSecondsLeft, setIsRunning } = useCountDown();
+
     return(
         <div>
             <h1>Timer page</h1>
-            <button  onclick="OnTimerStarted">Start Timer</button>
+            <p> work {secondsLeft} seconds</p>
+            <button onClick={() => startTimer(setSecondsLeft, setIsRunning, 60)}>Start Timer</button>
+            <button onClick={() => pauseTimer(setIsRunning)}> pause Timer </button>
         </div>
     );
 }
