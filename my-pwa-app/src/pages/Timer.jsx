c9f665f078cc8316a03c94d7afbff9e0b655dae4
Roanbradley <roanbradley5@gmail.com>
2026-05-09 21:30:49 +0100

    messing with play button

--- my-pwa-app/src/pages/Timer.jsx
+++ my-pwa-app/src/pages/Timer.jsx
@@ -2,6 +2,7 @@
 import {pauseTimer, startTimer, useCountDown, formatTime, resetTimer} from "../Logic/TimerLogic";
 import { useState, useEffect } from "react";
 import Stack from "@mui/material/Stack";
+import playbutton from '../assets/play_button1.jpg'
 
 export default function Timer(){
 
@@ -13,11 +14,14 @@ export default function Timer(){
             <p className = "seconds">{formatTime(secondsLeft)}</p>
 
             <Stack>
-            <button onClick={() => startTimer(setSecondsLeft, setIsRunning, secondsLeft)}>Start Timer</button>
-            <button onClick={() => pauseTimer(setIsRunning, running)}> pause Timer </button>
+            { /* <button onClick={() => startTimer(setSecondsLeft, setIsRunning, secondsLeft)}>Start Timer</button>*/ }
+            <button> <img className="playbutton"  src={'src/assets/play.png'} onClick={() => pauseTimer(setIsRunning, running)}/></button>
             <button onClick={() => resetTimer(setSecondsLeft, 10, setRounds, 3, setMode, "nuetral")}> reset Timer </button>
             <button> Rounds </button>
             </Stack>
+
+    
+           {/* <img className="playbutton" src={'src/assets/play.png'} />*/}
 
         </div>
     );
